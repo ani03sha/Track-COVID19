35192ce09f77139e80b2cda605f37796df230676
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-03-19 21:06:12 +0530

    Minor updates

--- track-covid19-ui/src/components/searchForm/SearchForm.js
+++ track-covid19-ui/src/components/searchForm/SearchForm.js
@@ -4,7 +4,7 @@ import './SearchForm.css';
 const SearchForm = ({ getCountryData }) => {
     return(
         <div className="search">
-            <h1>Track COVID19</h1>
+            <h1>Track CoronaVirus</h1>
             <div onSubmit={ getCountryData } className="search">
                 <form className = "search__form">
                     <input type="text" autoComplete="off" name="countryName" placeholder="Enter country name, e.g. India" />
